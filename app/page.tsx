c770d75ab22a1c59ab57ea0c42e43bd29f3da31b
kiritocode1 <kathawalearyan9@gmail.com>
2023-12-18 02:01:20 +0530

    Lucide and text upate

--- app/page.tsx
+++ app/page.tsx
@@ -61,14 +61,7 @@ export default function Home() {
 			<section
 				id="about"
 				className="h-fit min-h-screen w-full flex relative items-center justify-center p-8">
-				<div className="absolute -z-10 h-full w-full overflow-hidden">
-					<Image
-						src="/whirl.svg"
-						fill
-						className="absolute object-cover w-full overflow-visible sm:rotate-90"
-						alt="Background Whirl"
-					/>
-				</div>
+
 				<div className="w-full h-full flex items-center justify-center flex-col gap-8 max-w-7xl">
 					<h3 className="text-4xl md:text-5xl font-bold">Why Join US ???</h3>
 					<div className="w-full grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1 gap-4 justify-between relative">
