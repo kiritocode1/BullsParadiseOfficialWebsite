108fd84cf4a52512ae648e26de382b0c61258bf1
kiritocode1 <kathawalearyan9@gmail.com>
2023-12-21 12:37:03 +0530

    ADD : Slider.jsx

---
+++ components/Assetcompressor.ts
@@ -0,0 +1,21 @@
+import one from "./assets/1.png"; 
+import two from "./assets/2.png";
+import three from "./assets/3.png";
+import four from "./assets/4.png";
+import five from "./assets/5.png";
+import six from "./assets/6.png";
+import seven from "./assets/7.png";
+import eight from "./assets/8.png";
+
+const images = [
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight
+]
+
+export default images;
